306e0c071f9ef75a053769be1c17b11e0a9e6543
RadoRado <radorado@hackbulgaria.com>
2015-04-19 18:50:23 +0300

    If files are there, remove and replace

--- setup.py
+++ setup.py
@@ -1,16 +1,20 @@
 import os
 from glob import glob
-from os.path import expanduser
+from os.path import expanduser, isfile
 
 def main():
     print("Setup script for dotfiles.")
     rc_files = glob("*rc")
     cwd = os.getcwd()
     home = expanduser("~")
-    
     for rc in rc_files:
         here = cwd + "/" + rc
         there = home + "/." + rc
+
+        if isfile(there):
+            print("{} exists".format(there))
+            print("Removing and replacing")
+            os.remove(there)
 
         print("Symlinking {} to {}".format(here, there))
         os.symlink(here, there)
