0186478bf518d564c3dd62ed16f11a9d4a5a395a
Radoslav Georgiev <radorado@hacksoft.io>
2024-12-19 11:47:41 +0200

    Improve telescope config

--- nvim/lua/radorado/plugins/telescope.lua
+++ nvim/lua/radorado/plugins/telescope.lua
@@ -2,12 +2,38 @@ return {
     {
         'nvim-telescope/telescope.nvim', tag = '0.1.8',
         -- or                              , branch = '0.1.x',
-        dependencies = { 'nvim-lua/plenary.nvim' },
+        dependencies = { 
+            {'nvim-lua/plenary.nvim'},
+            {'nvim-telescope/telescope-fzf-native.nvim', build = 'make'}
+        },
         config = function()
             local builtin = require('telescope.builtin')
-            vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telescope find files' })
-            vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Telescope find files' })
-            vim.keymap.set('n', '<leader>ps', builtin.live_grep, { desc = 'Telescope live grep' })
+            local actions = require("telescope.actions")
+
+            -- We want to close telescope even when we are in insert mode there
+            -- https://github.com/nvim-telescope/telescope.nvim/wiki/Configuration-Recipes#mapping-esc-to-quit-in-insert-mode
+            require("telescope").setup({
+                defaults = {
+                    mappings = {
+                        i = {
+                            ["<esc>"] = actions.close
+                        }
+                    }
+                }
+            })
+
+            -- vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Telescope find files' })
+            vim.keymap.set('n', '<C-p>', function()
+                local ok, _ = pcall(builtin.git_files)
+
+                if not ok then
+                    builtin.find_files()
+                end
+
+            end, { desc = 'Telescope find files' })
+
+            -- vim.keymap.set('n', '<C-F>', builtin.find_files, { desc = 'Telescope find files' })
+            vim.keymap.set('n', '<C-s>', builtin.live_grep, { desc = 'Telescope live grep' })
         end,
     }
 }
